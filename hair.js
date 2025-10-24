ef25b92e4c7235ed69eb575ea2ecb7b7f3d7a52c
Human-AI Relationality (HAIR) <support@humanairelationality.org>
2025-10-23 23:48:31 -0400

    Add files via upload

---
+++ hair.js
@@ -0,0 +1,9 @@
+ 
+  document.addEventListener("DOMContentLoaded", () => {
+
+  const toggle = document.querySelector('.nav-toggle');
+  const links = document.querySelector('.nav-links');
+  toggle.addEventListener('click', () => {
+    links.classList.toggle('active');
+  });
+});
